e9f60cc7f969c93f634bc17f293dcef421e5f0cf
wallace <wallace_wolv@hotmail.com>
2023-05-17 19:29:02 -0300

    feat: Blur background e Stripes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         sans: 'var(--font-roboto)',
         alt: 'var(--font-bai-jamjuree)',
       },
+
       colors: {
         gray: {
           50: '#eaeaea',
@@ -72,6 +73,19 @@ module.exports = {
           800: '#8c6923',
           900: '#6b501b',
         },
+      },
+
+      backgroundImage: {
+        stripes:
+          'linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)',
+      },
+
+      backgroundSize: {
+        stripes: '100% 8px',
+      },
+
+      blur: {
+        full: '194px',
       },
     },
   },
